5334d6b3d8bbdcd7ee3050173709a86b88c2bdf5
zbeast <zbeast@gmail.com>
2024-04-02 19:58:18 -0700

    this has how to setup ssh

--- dummy.py
+++ dummy.py
@@ -1,3 +1,7 @@
 print("hello world")
 print("more shit")
 
+#You may also need to update the origin for your repository if it is set to HTTPS. Do this to switch to SSH:
+
+#git remote -v
+#git remote set-url origin git@github.com:USERNAME/REPONAME.git
